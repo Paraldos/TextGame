07a78da307af1a4730dd441261180e53ae60b8fc
Schiggy <hillbrecht.s@gmail.com>
2021-01-24 12:05:57 +0100

    basics
    basics

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-console.log("Hallo Welt")
-
+/* variables */
+let navBox = document.getElementsByClassName("navBox");
+let gameBox = document.querySelector(".gameBox");
 let player = {
-    strength: 0
+  strength: 0,
+};
+
+function clearGameBox() {
+  while (gameBox.children.length > 0) gameBox.lastChild.remove();
 }
+
+function fillGameBox(array) {
+  array.forEach((element) => {
+    if (element.x) {
+      console.log("Juhu");
+    }
+    console.log(element);
+  });
+}
+
+clearGameBox();
+fillGameBox([{ x: true }, "b", "c"]);
