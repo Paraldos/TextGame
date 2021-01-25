e8ba9468669f49036695cbe21277b157593091d2
Schiggy <hillbrecht.s@gmail.com>
2021-01-25 11:39:40 +0100

    changescene
    changescene

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 /* variables */
 let navBox = document.getElementsByClassName("navBox");
 let gameBox = document.querySelector(".gameBox");
+let btnNumber;
 let player = {
   strength: 0,
+  switch_01: true,
 };
 
 let scenes = {
+  // ==================
   start: [
     { typ: "txt", txt: "Scene: Start" },
     { typ: "txt", txt: "Hier steht jede Menge Text! Ist das nicht aufregend?" },
     { typ: "btn", txt: "Gehe zur Scene: Test_01", changeScene: "Test_01" },
+    { typ: "txt", txt: "Test Switch_01", switch: player.switch_01 },
+    { typ: "btn", txt: "Gehe zur Scene: Test_02", changeScene: "Test_02" },
   ],
+  // ==================
   Test_01: [
     { typ: "txt", txt: "Scene: Test_01" },
+    { typ: "btn", txt: "Gehe zurück zur Scene: Start", changeScene: "start" },
+  ],
+  // ==================
+  Test_02: [
+    { typ: "txt", txt: "Scene: Test_02" },
     { typ: "btn", txt: "Gehe zurück zur Scene: Start", changeScene: "start" },
   ],
 };
 
 function fillGameBox(scene) {
-  // clear current screen
+  btnNumber = 1;
   while (gameBox.children.length > 0) gameBox.lastChild.remove();
-  // fill in new stuff
   scene.forEach((sceneElement) => createHTMLElements(sceneElement));
 }
 
 function createHTMLElements(sceneElement) {
+  if (sceneElement.switch == false) return;
   let newDiv = document.createElement("div");
   newDiv.classList.add(sceneElement.typ);
-  if (sceneElement.txt) newDiv.innerText = sceneElement.txt;
-  if (sceneElement.typ == "btn")
+  if (sceneElement.typ == "txt") newDiv.innerText = sceneElement.txt;
+  if (sceneElement.typ == "btn") {
+    newDiv.innerText = btnNumber + ".) " + sceneElement.txt;
+    btnNumber++;
     newDiv.addEventListener("click", () => clickBtn(sceneElement));
+  }
   // append child
   gameBox.appendChild(newDiv);
 }
